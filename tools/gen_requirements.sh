e2624086e4ba7a8fc27686bd8709eb6b53e6d538
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2021-10-17 13:54:57 -0700

    tools: ensure gen_requirements remains pure

--- tools/gen_requirements.sh
+++ tools/gen_requirements.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-./env/bin/pip freeze -r requirements.in -l > requirements.txt
+set -euo pipefail
+
+TMP_VIRTUALENV="$(mktemp -d)"
+PIP_BIN="$TMP_VIRTUALENV/bin/pip"
+REQ_IN="$REPO_ROOT/requirements.in"
+REQ_TXT="$REPO_ROOT/requirements.txt"
+
+python3 -m venv "$TMP_VIRTUALENV"
+
+"$PIP_BIN" install -r "$REQ_IN"
+"$PIP_BIN" freeze -r "$REQ_IN" -l > "$REQ_TXT"
+
+rm -rf "$TMP_VIRTUALENV"
